38faf6d8ec3f4e1a71c474392482a2f4b850a92d
vandersonmr <vandersonmr2@gmail.com>
2019-03-04 14:30:09 +0000

    Adding args

--- capsnet.py
+++ capsnet.py
@@ -11,28 +11,33 @@ from capslayer import *
 
 K.set_image_data_format('channels_last')
 
-def CapsNet(input_shape, n_class, routings, lanes = 4, lanesize = 1, depth = 1):
+def Lane(laneID, n_class, lanesize, lanetype, lane_input, routings):
+    if (lanetype == 2):
+        output = layers.Conv2D(filters=lanesize*16, kernel_size=1, strides=1, padding='same', name='convs1-'+str(laneID))(lane_input)
+        output = layers.SeparableConv2D(filters=lanesize*16, kernel_size=6, strides=1, padding='valid', activation='relu', name='convs'+str(laneID))(output)
+        for j in range(3):
+            output = layers.Conv2D(filters=lanesize*16, kernel_size=3, strides=1, padding='same', activation='relu')(output)
+    else:
+        output = layers.Conv2D(filters=lanesize*16, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1'+str(laneID))(lane_input)
+
+    convs = output
+
+    primarycaps = PrimaryCap(convs, dim_capsule=16, n_channels=lanesize*2, kernel_size=6, strides=2, padding='valid', i = laneID)
+    
+    digitcaps = CapsuleLayer(num_capsule=1, dim_capsule=n_class, routings=routings, batchsize = args.batch_size, name='digitcaps'+str(laneID))(primarycaps)
+
+    return digitcaps
+
+def LaneCapsNet(input_shape, n_class, routings, num_lanes = 4, lanesize = 1, lanetype = 1):
     x = layers.Input(shape=input_shape)
 
-    convs = [];
-    for i in range(0,lanes):
-      output = layers.Conv2D(filters=lanesize*16, kernel_size=1, strides=1, padding='same', name='convs1-'+str(i))(x)
-      output = layers.SeparableConv2D(filters=lanesize*16, kernel_size=6, strides=1, padding='valid', activation='relu', name='convs'+str(i))(output)
-      for j in range(3):
-        output = layers.Conv2D(filters=lanesize*16, kernel_size=3, strides=1, padding='same', activation='relu')(output)
-      convs = convs + [output]
+    lanes = []
+    for i in range(0, num_lanes):
+        lanes = lanes + [Lane(i, n_class, lanesize, lanetype, x, routings)]
 
-    primarycaps = [];
-    for i in range(0,lanes):
-        primarycaps = primarycaps + [PrimaryCap(convs[i], dim_capsule=16, n_channels=lanesize*2, kernel_size=6, strides=2, padding='valid', i = i)]
-    
-    digitcapsl = [];
-    for i in range(0,lanes):
-        digitcapsl = digitcapsl + [CapsuleLayer(num_capsule=1, dim_capsule=n_class, routings=routings, batchsize = args.batch_size, name='digitcaps'+str(i))(primarycaps[i])]
+    digitcaps1 = Lambda(lambda ls : K.permute_dimensions(concatenate(ls, axis=1), [0,2,1]))(lanes)
 
-    digitcaps1 = Lambda(lambda ls : K.permute_dimensions(concatenate(ls, axis=1), [0,2,1]))(digitcapsl)
-
-    digitcaps =  layers.Dropout(args.dropoutrate, (1, lanes))(digitcaps1)
+    digitcaps = layers.Dropout(args.dropout, (1, lanes))(digitcaps1)
 
     out_caps = Length(name='capsnet')(digitcaps)
 
@@ -43,7 +48,7 @@ def CapsNet(input_shape, n_class, routings, lanes = 4, lanesize = 1, depth = 1):
 
     # Shared Decoder model in training and prediction
     decoder = models.Sequential(name='decoder')
-    decoder.add(layers.Dense(512, activation='relu', input_dim=lanes*n_class))
+    decoder.add(layers.Dense(512, activation='relu', input_dim=num_lanes*n_class))
     decoder.add(layers.Dense(1024, activation='relu'))
     decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
     decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
@@ -53,34 +58,19 @@ def CapsNet(input_shape, n_class, routings, lanes = 4, lanesize = 1, depth = 1):
     eval_model = models.Model(x, [out_caps, decoder(masked)])
 
     # manipulate model
-    noise = layers.Input(shape=(n_class, lanes))
+    noise = layers.Input(shape=(n_class, num_lanes))
     noised_digitcaps = layers.Add()([digitcaps1, noise])
     masked_noised_y = Mask()([noised_digitcaps, y])
     manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
     return train_model, eval_model, manipulate_model
-
   
 def margin_loss(y_true, y_pred):
-    """
-    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
-    :param y_true: [None, n_classes]
-    :param y_pred: [None, num_capsule]
-    :return: a scalar loss value.
-    """
     L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
         0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
 
     return K.mean(K.sum(L, 1))
 
-
 def train(model, data, args):
-    """
-    Training a CapsuleNet
-    :param model: the CapsuleNet model
-    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
-    :param args: arguments
-    :return: The trained model
-    """
     # unpacking the data
     (x_train, y_train), (x_test, y_test) = data
 
@@ -96,36 +86,15 @@ def train(model, data, args):
             loss=[margin_loss, 'mse'],
             loss_weights=[1., args.lam_recon],
             metrics={'capsnet': 'accuracy'})
-
     
     # Training without data augmentation:
     model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
               validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint, lr_decay])
-    """
-
-    # Begin: Training with data augmentation ---------------------------------------------------------------------#
-    def train_generator(x, y, batch_size, shift_fraction=0.):
-        train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
-                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
-        generator = train_datagen.flow(x, y, batch_size=batch_size)
-        while 1:
-            x_batch, y_batch = generator.next()
-            yield ([x_batch, y_batch], [y_batch, x_batch])
-
-    # Training with data augmentation. If shift_fraction=0., also no augmentation.
-    model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
-                        steps_per_epoch=int(y_train.shape[0] / args.batch_size),
-                        epochs=args.epochs,
-                        validation_data=[[x_test, y_test], [y_test, x_test]],
-                        callbacks=[log, tb, checkpoint, lr_decay])
-    # End: Training with data augmentation -----------------------------------------------------------------------#
-    """
 
     model.save_weights(args.save_dir + '/trained_model.h5')
     print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
 
     return model
-
 
 def test(model, data, args):
     x_test, y_test = data
@@ -141,7 +110,6 @@ def test(model, data, args):
     print('-' * 30 + 'End: test' + '-' * 30)
     plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
     plt.show()
-
 
 def manipulate_latent(model, data, args):
     print('-'*30 + 'Begin: manipulate' + '-'*30)
@@ -167,7 +135,6 @@ def manipulate_latent(model, data, args):
     print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
     print('-' * 30 + 'End: manipulate' + '-' * 30)
 
-
 def load_cifar():
     # the data, shuffled and split between train and test sets
     #from keras.datasets import fashion_mnist
@@ -191,44 +158,56 @@ def load_mnist():
     y_train = to_categorical(y_train.astype('float32'))
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
+if __name__ == "__main__":
 
+    import os
+    import argparse
+    from keras.preprocessing.image import ImageDataGenerator
+    from keras import callbacks
 
-import os
-import argparse
-from keras.preprocessing.image import ImageDataGenerator
-from keras import callbacks
+    parser = argparse.ArgumentParser(description="Multi-lane Capsule Network")
 
-# setting the hyper parameters
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch_size', default=100, type=int)
+    parser.add_argument('--lr', default=0.001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--lam_recon', default=0.392, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('-r', '--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")
+    parser.add_argument('--shift_fraction', default=0.1, type=float,
+                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--digit', default=5, type=int,
+                        help="Digit to manipulate")
+    parser.add_argument('--dropout', default=0, type=float,
+                        help="Percentage of lanes to be dropout per batch")
+    parser.add_argument('--num_lanes', default=4, type=int,
+                        help="Number of lanes")
+    parser.add_argument('--lane_size', default=1, type=int,
+                        help="Lane size")
+    parser.add_argument('--lane_type', default=1, type=int,
+                        help="Type of the lane")
+    parser.add_argument('-w', '--weights', default=None, help="The path of the saved weights. Should be specified when testing")
+    parser.add_argument('--dataset', default='mnist')
+    args = parser.parse_args()
 
-class A:
-  pass
-  
-for l in [4]: #[2, 4, 8, 16, 32]:
-    for s in [1]: #[1,2,4]:
-        args = A()
-        args.digit = 5
-        args.epochs = 20
-        args.batch_size = 100
-        args.lr = 0.01
-        args.lr_decay = 0.9
-        args.lam_recon = 0.392
-        args.routings = 3
-        args.shift_fraction = 0.0
-        args.save_dir = "./mnist-result-l"+str(l)+"-s"+str(s)+"/"
-        args.debug = False
-        args.dropoutrate = 0.0
-        regularizers.l1_l2(l1=0.008, l2=0.008)
+    regularizers.l1_l2(l1=0.008, l2=0.008)
+    
+    (x_train, y_train), (x_test, y_test) = load_mnist() if args.dataset == "mnist" else load_cifar()
 
-        # load data
-        (x_train, y_train), (x_test, y_test) = load_mnist()
+    model, eval_model, manipulate_model = LaneCapsNet(input_shape=x_train.shape[1:],
+                                                    n_class=len(np.unique(np.argmax(y_train, 1))),
+                                                    routings=args.routings,
+                                                    num_lanes = args.num_lanes,
+                                                    lanesize = args.lane_size,
+                                                    lanetype = args.lane_type)
+    model.summary()
 
-        # define model
-        model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
-                                                      n_class=len(np.unique(np.argmax(y_train, 1))),
-                                                      routings=args.routings,
-                                                      lanes = l,
-                                                      lanesize = s,
-                                                      depth = l)
-        model.summary()
-
-        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
+    train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
